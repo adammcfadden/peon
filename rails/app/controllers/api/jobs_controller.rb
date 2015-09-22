39845a4cda0d1d81497d28239f8a085c4682a85a
Adam <amcfadden@renewfund.com>
2015-09-22 10:49:43 -0700

    add create route for rails

--- rails/app/controllers/api/jobs_controller.rb
+++ rails/app/controllers/api/jobs_controller.rb
@@ -3,7 +3,21 @@ class Api::JobsController < ApplicationController
     render json: Job.all
   end
 
+  def create
+    job = Job.new(job_params)
+    if job.save
+      render json: job, status: 200
+    else
+      render json: job, status: 422
+    end
+  end
+
   def show
     render json: Job.find(params[:id])
+  end
+
+  private
+  def job_params
+    params.require(:job).permit(:position, :company)
   end
 end
